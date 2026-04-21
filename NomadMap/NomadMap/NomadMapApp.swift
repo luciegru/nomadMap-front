8bea7fb99ee664e121e2d48ac9f80600ea2ea33d
Lucie Grunenberger <lucie.grunenberger@gmail.com>
2026-04-21 13:40:28 +0200

    Initial Commit

---
+++ NomadMap/NomadMap/NomadMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomadMapApp.swift
+//  NomadMap
+//
+//  Created by Lucie Grunenberger  on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NomadMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
